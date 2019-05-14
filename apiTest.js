7ca5c1ccdbec8cd7513c7c94c5a156fb24b81f40
Ramu Reddy <ramu@piktorlabs.com>
2019-05-14 15:26:06 +0530

    stage

---
+++ apiTest.js
@@ -0,0 +1,37 @@
+window.SPECS_CONFIG = [ {
+  "title": "B2BExperience",
+  "id": "B2BExperience",
+  "url": "https://productrealization.github.io/devportal/API Basics/basics.yaml"
+  "items": [
+    {
+      "title": "BillingAccounts",
+      "id": "B2BExperience_BillingAccounts",
+      "versions": [
+        {
+          "name": "v2",
+          "url": "https://productrealization.github.io/devportal/B2BExperience/BillingAccounts/v2/b2b-experience-billing-accounts.json"
+        }
+      ]
+    },
+    {
+      "title": "Products",
+      "id": "B2BExperience_Products",
+      "versions": [
+        {
+          "name": "v2",
+          "url": "https://productrealization.github.io/devportal/B2BExperience/Products/v2/b2b-experience-products.json"
+        }
+      ]
+    },
+    {
+      "title": "TransactionService",
+      "id": "B2BExperience_TransactionService",
+      "versions": [
+        {
+          "name": "v1",
+          "url": "https://productrealization.github.io/devportal/B2BExperience/TransactionService/v1/b2b-experience-transaction-service.json"
+        }
+      ]
+    }
+  ]
+}];
